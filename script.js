0bf3dfb67f08f69eefd1cc20db67e7c119fce6d1
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2023-10-10 13:33:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "hello, world !";
-/*
-Tout ce qui est écrit ici est entre commentaires.
-*/
-// Voici un commentaire sur une ligne
-
-let iceCream = "chocolat";
-if (iceCream === "chocolat") {
-  alert("J'adore la glace au chocolat !");
-} else {
-  alert("Ooooh, mais j'aurais préféré au chocolat.");
-}
-document.querySelector("html").addEventListener("click", function () {
-    alert("Aïe, arrêtez de cliquer !!");
-  });
-  let myImage = document.querySelector("img");
-
-myImage.addEventListener("click", function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "/media/page-daccueil.jpg") {
-    myImage.setAttribute("src", "media/page-daccueil.jpg");
-  } else {
-    myImage.setAttribute("src", "media/firefox_2.jpg");
+function setUserName() {
+    let myName = prompt("Veuillez saisir votre nom.");
+    localStorage.setItem("nom", myName);
+    myHeading.textContent = "Mozilla est cool, " + myName;
   }
-});
-
+  if (!localStorage.getItem("nom")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("nom");
+    myHeading.textContent = "Mozilla est cool, " + storedName;
+  }
+  myButton.addEventListener("click", function () {
+    setUserName();
+  });    
